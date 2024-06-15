fa7dc6eeeaf6f1bd21d8c38b683bcc2bf496040e
jorgefr-blip <jfrancoh3@miumg.edu.gt>
2024-06-15 01:50:06 -0600

    carga proyecto

---
+++ src/main/java/com/mycompany/logina/LoginA.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.logina;
+
+import java.util.ArrayList;
+
+/**
+ *+
+ * @author jorge
+ */
+public class LoginA {
+
+    public static ArrayList<Profesor> profesores = new ArrayList<>();
+    public static profesor profesorLogeado = new profesor(); 
+    public static void main(String[]args ) {
+        Login 1 = new Login(); 
+        1.setVisible(true); 
+    }
+    
+    
+}
